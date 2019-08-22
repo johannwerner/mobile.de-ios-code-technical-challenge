fda581fd4b7af9b5bb0469edac9682c9da5ce643
Johann Werner <johann@wunder.org>
2019-08-22 15:56:46 +0200

    Remove header

--- mobileTests/mobileTests.swift
+++ mobileTests/mobileTests.swift
@@ -1,11 +1,3 @@
-//
-//  CarAppTests.swift
-//  CarAppTests
-//
-//  Created by Johann Werner on 16.08.19.
-//  Copyright © 2019 Johann Werner. All rights reserved.
-//
-
 import XCTest
 @testable import mobile
 
